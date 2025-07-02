96816dfe70cab6e1bc4d174e8e51b68d49ce9995
yxxhero <aiopsclub@163.com>
2025-07-02 08:49:54 +0800

    feat: Add lxcfs-remount script and set executable
    Signed-off-by: yxxhero <aiopsclub@163.com>

---
+++ scripts/lxcfs-remount.sh
@@ -0,0 +1,28 @@
+#!/bin/bash +x
+LXCFS="$1"
+containers=$(crictl ps | grep -v pause | grep -v calico | grep -v cilium | awk '{print $1}' | grep -v CONTAINER)
+for container in $containers; do
+    # Get the container's mounts
+    mounts=$(crictl inspect -o go-template --template='{{range .info.config.mounts}}{{.container_path}} -> {{.host_path}}{{println}}{{end}}' $container | grep "$LXCFS/")
+    
+    echo "Mounts for container $container:"
+    echo "$mounts"
+    
+    # Check if the container has the LXCFS mount
+    count=$(echo "$mounts" | grep  "$LXCFS/" | wc -l)
+    if [ "$count" != "0" ]; then
+        echo "remount $container"
+        PID=$(crictl inspect --output go-template --template '{{- .info.pid -}}' $container)
+        # mount /proc
+        for file in meminfo cpuinfo loadavg stat diskstats swaps uptime; do
+            echo nsenter --target $PID --mount -- mount -B "$LXCFS/proc/$file" "/proc/$file"
+            nsenter --target $PID --mount -- mount -B "$LXCFS/proc/$file" "/proc/$file"
+        done
+
+        echo nsenter --target $PID --mount -- mount -B "$LXCFS/sys/devices/system/cpu" "/sys/devices/system/cpu"
+        nsenter --target $PID --mount -- mount -B "$LXCFS/sys/devices/system/cpu" "/sys/devices/system/cpu"
+    else
+        echo "No LXCFS mount found for container $container"
+    fi
+done
+exit 0
